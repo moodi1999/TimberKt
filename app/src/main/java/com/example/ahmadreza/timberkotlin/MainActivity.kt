6b825c67b2d988defa0af63be840f0c5c64c502a
Ahmadreza Moodi <moodi1378@gmail.com>
2018-11-22 19:32:37 +0330

    added the res and empty package

--- app/src/main/java/com/example/ahmadreza/timberkotlin/MainActivity.kt
+++ app/src/main/java/com/example/ahmadreza/timberkotlin/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ahmadreza.timberkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+
+        when (id) {
+            R.id.action_setting -> {
+                return true
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
